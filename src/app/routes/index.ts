1fb66d509f7932c4e58c9fc1f42a138cb0b1ae04
BappySheikh1 <sheikhbappy91gmail.com>
2023-09-02 00:54:42 +0600

    user update and delete

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,22 +2,27 @@ import express from 'express';
 import { authRoutes } from '../modules/auth/auth.route';
 import { BookRoutes } from '../modules/Books/Book.route';
 import { UserRoutes } from '../modules/User/user.route';
+import { CategoryRoute } from '../modules/Category/category.route';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: "/auth",
-    routes: authRoutes
+    path: '/auth',
+    routes: authRoutes,
   },
   {
-    path: "/users",
-    routes: UserRoutes
+    path: '/users',
+    routes: UserRoutes,
   },
   {
-    path: "/books",
-    routes: BookRoutes
+    path: '/categories',
+    routes: CategoryRoute,
+  },
+  {
+    path: '/books',
+    routes: BookRoutes,
   },
 ];
 
