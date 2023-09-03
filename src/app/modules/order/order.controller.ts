3675a3203b1af820f9daaf5f97ccce6a24270694
BappySheikh1 <sheikhbappy91gmail.com>
2023-09-03 21:30:40 +0600

    modify added

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -5,44 +5,51 @@ import sendResponse from '../../../shared/sendResponse';
 import httpStatus from 'http-status';
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
-  const { userId } = req.user as { userId: string };
-  const books = req.body;
 
-  const result = await OrderService.insertIntoDB(books, userId);
+
+  const { userId } = req.user as { userId: string };
+  const products = req.body;
+
+
+  const result = await OrderService.insertIntoDB(products, userId);
 
   sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: 'Order created successfully',
-    data: result,
-  });
+       statusCode: httpStatus.OK,
+       success: true,
+       message: "Order created successfully",
+       data: result
+  })
 });
 
+
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
-  const { userId, role } = req.user as { userId: string; role: string };
+
+  const { userId, role } = req.user as { userId: string, role: string };
 
   const result = await OrderService.getAllFromDB(userId, role);
 
   sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: 'Orders fetched successfully',
-    data: result,
-  });
+       statusCode: httpStatus.OK,
+       success: true,
+       message: "Orders fetched successfully",
+       data: result
+  })
 });
+
 
 const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
   const { orderId } = req.params;
-  const { userId, role } = req.user as { userId: string; role: string };
+  const { userId, role } = req.user as { userId: string, role: string };
+
 
   const result = await OrderService.getByIdFromDB(orderId, userId, role);
 
   sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: 'Order fetched successfully',
-    data: result,
-  });
+       statusCode: httpStatus.OK,
+       success: true,
+       message: "Order fetched successfully",
+       data: result
+  })
 });
 
 export const OrderController = {
